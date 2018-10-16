69b4fc09ee98210280a02b1e60b8b28bfc3cdb30
Matte <mattias.johansson@ivbar.com>
2018-10-16 09:09:29 +0200

    Added more information from the mails.

--- 2018-10-11.py
+++ 2018-10-11.py
@@ -1,6 +1,11 @@
 """A.k.a. subset sum problem.
 
-    bonus: Do it in one pass.
+This problem was recently asked by Google.
+
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k. For example, given
+10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
 """
 
 
